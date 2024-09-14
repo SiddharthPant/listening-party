0d9da1e597f0663db4c407680908e4beca31d606
SiddharthPant <spsiddharthpant@gmail.com>
2024-09-14 11:37:01 +0530

    Create your own dashboard blade template

---
+++ resources/views/livewire/dashboard.blade.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Models\ListeningParty;
+use Livewire\Volt\Component;
+
+new class extends Component {
+    public string $name = '';
+    public $startTime;
+    public function createListeningParty()
+    {
+
+    }
+    public function with()
+    {
+        return [
+            'listening_parties' => ListeningParty::all(),
+        ];
+    }
+}; ?>
+
+<div class="flex items-center justify-center min-h-screen bg-slate-50">
+    <div class="max-w-lg w-full px-4">
+        <form wire:submit="createListeningParty" class="space-y-6">
+            <x-input wire:model="name" placeholder="Listening Party Name" />
+            <x-datetime-picker wire:model="startTime" placeholder="Start Time" />
+            <x-button primary>Create Listening Party</x-button>
+        </form>
+    </div>
+</div>
